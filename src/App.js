08e6f064c3eb81a677ae3b6b5b9a189db3505544
manishmasand1234 <manishsinghmasand@gmail.com>
2025-02-08 18:33:37 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Component/LoginPage';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    
     <div>
+      <BrowserRouter basename='/formvalidation'>
       <LoginPage />
+      </BrowserRouter>
+      
     </div>
   );
 }
